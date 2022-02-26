61b5fa3f581b4974bef49901e862b43541f9277a
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-02-26 19:05:20 +0100

    Aggiunti colori

--- src/printer.ml
+++ src/printer.ml
@@ -1,3 +1,20 @@
+type colore = Colore of string;;
+
+
+(*colori*)
+let rosso = Colore "\027[31m";;
+let verde = Colore "\027[32m";;
+let ciano =  Colore "\027[36m";;
+let bianco = Colore "\027[0m";;
+let rosso_b = Colore "\027[31;1m";;
+let verde_b = Colore "\027[32;1m";;
+let ciano_b = Colore "\027[36;1m";;
+let bianco_b = Colore "\027[0;1m";;
+
+let reset = bianco;;
+
+
+
 let stampa_logo () = 
   let aux () = print_string 
 "
@@ -19,6 +36,23 @@ let stampa_logo () =
 ;;
 
 
+(*Stampa il testo colorato*)
+(* Prende un colroe e del testo. Stampa il testo col determinato colore
+
+  input:
+    colore: Colore
+    testo:  Stringa da stampare
+*)
+let print_colore (Colore colore) testo = 
+  let aux (Colore colore) testo (Colore reset) = 
+    print_string (colore ^ testo ^ reset)
+  in aux (Colore colore) testo reset;;
+
+
+
+
+
+
 (*Stampa la lista di nodi colorati*)
 (* Questa lista si ottiene dall'esecuzione delle fuanzione colora.
     Stampa tutti gli elementi della lista di colori con Nodo: x - Colore: y
@@ -36,8 +70,8 @@ let stampa_nodi_colorati colorati =
   let rec aux = function    (*lista di nodi colorati*)
     []              -> print_string "\n"  (*caso base, la lista è vuota*)
     | (x, y)::rest  ->                    (*caso ricorsivo, prende il primo elemento e lo stampa*)
-      print_string "Nodo: "; print_int x;
-      print_string " - Colore: "; print_int y; print_string "\n";
+      print_colore ciano_b "Nodo: "; print_int x;
+      print_string " - "; print_colore rosso_b "Colore: "; print_int y; print_string "\n";
 
       aux rest;
   in aux colorati  (*avvia la ricorsione con lista_di_nodi_colorati=colorati*)
